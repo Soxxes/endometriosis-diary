7357299a769806e88c32ec9ae16591d0eebf6449
Soxxes <hscschwimmermarc@t-online.de>
2024-08-29 14:24:26 +0200

    added user login

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from werkzeug.security import check_password_hash
 from bson.objectid import ObjectId
 from backend.extensions import mongo
 from backend.models.user import User
@@ -8,13 +9,26 @@ users = Blueprint('users', __name__)
 
 @users.route('/add', methods=['POST'])
 def add_user():
-    user_data = request.json
+    data = request.json
 
-    validated_data, errors = User.validate(user_data)
+    validated_data, errors = User.validate(data)
     if errors:
         return jsonify({"errors": errors}), 400
     
     user = User(**validated_data)
     user_id = user.save()
-    
+
     return jsonify({"message": "User added", "id": str(user_id)}), 201
+
+
+@users.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    user = mongo.db.users.find_one({"username": username})
+    
+    if user and check_password_hash(user['password'], password):
+        return jsonify({"message": "Login successful"}), 200
+    return jsonify({"message": "Invalid credentials"}), 401
